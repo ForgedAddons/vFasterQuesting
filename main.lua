66493b112f9c0c53dc320fbfad2ea5e5e6a04bc7
V. <bartosz.bentkowski@gmail.com>
2014-01-04 13:28:45 +0100

    bugfixes

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local addon, ns = ...
 
-ns.completed_quests = {}
-ns.uncompleted_quests = {}
+local completed_quests = {}
+local uncompleted_quests = {}
 
 function ns:canAutomate ()
 	if IsShiftKeyDown() then
@@ -21,22 +21,22 @@ function ns:strip_text (text)
 end
 
 ns.RegisterEvent("QUEST_PROGRESS", function()
-	if not self:canAutomate() then return end
+	if not ns.canAutomate() then return end
 	if IsQuestCompletable() then
 		CompleteQuest()
 	end
 end)
 
 ns.RegisterEvent("QUEST_LOG_UPDATE", function()
-	if not self:canAutomate() then return end
+	if not ns.canAutomate() then return end
 	local start_entry = GetQuestLogSelection()
 	local num_entries = GetNumQuestLogEntries()
 	local title
 	local is_complete
 	local no_objectives
 
-	self.completed_quests = {}
-	self.uncompleted_quests = {}
+	completed_quests = {}
+	uncompleted_quests = {}
 
 	if num_entries > 0 then
 		for i = 1, num_entries do
@@ -44,10 +44,10 @@ ns.RegisterEvent("QUEST_LOG_UPDATE", function()
 			title, _, _, _, _, _, is_complete = GetQuestLogTitle(i)
 			no_objectives = GetNumQuestLeaderBoards(i) == 0
 			if title and (is_complete or no_objectives) then
-				self.completed_quests[title] = true
+				completed_quests[title] = true
 			else
 				title = title or ''
-				self.uncompleted_quests[title] = true
+				uncompleted_quests[title] = true
 			end
 		end
 	end
@@ -56,7 +56,7 @@ ns.RegisterEvent("QUEST_LOG_UPDATE", function()
 end)
 
 ns.RegisterEvent("GOSSIP_SHOW", function()
-	if not self:canAutomate() then return end
+	if not ns.canAutomate() then return end
 
 	local button
 	local text
@@ -64,11 +64,11 @@ ns.RegisterEvent("GOSSIP_SHOW", function()
 	for i = 1, 32 do
 		button = _G['GossipTitleButton' .. i]
 		if button:IsVisible() then
-			text = self:strip_text(button:GetText())
+			text = ns.strip_text(button:GetText())
 			if button.type == 'Available' then
 				button:Click()
 			elseif button.type == 'Active' then
-				if self.completed_quests[text] then
+				if completed_quests[text] then
 					button:Click()
 				end
 			end
@@ -77,7 +77,7 @@ ns.RegisterEvent("GOSSIP_SHOW", function()
 end)
 
 ns.RegisterEvent("QUEST_GREETING", function()
-	if not self:canAutomate() then return end
+	if not ns.canAutomate() then return end
 
 	local button
 	local text
@@ -85,10 +85,10 @@ ns.RegisterEvent("QUEST_GREETING", function()
 	for i = 1, 32 do
 		button = _G['QuestTitleButton' .. i]
 		if button:IsVisible() then
-			text = self:strip_text(button:GetText())
-			if self.completed_quests[text] then
+			text = ns.strip_text(button:GetText())
+			if completed_quests[text] then
 				button:Click()
-			elseif not self.uncompleted_quests[text] then
+			elseif not uncompleted_quests[text] then
 				button:Click()
 			end
 		end
@@ -96,12 +96,12 @@ ns.RegisterEvent("QUEST_GREETING", function()
 end)
 
 ns.RegisterEvent("QUEST_DETAIL", function()
-	if not self:canAutomate() then return end
+	if not ns.canAutomate() then return end
 	AcceptQuest()
 end)
 
 ns.RegisterEvent("QUEST_COMPLETE", function()
-	if not self:canAutomate() then return end
+	if not ns.canAutomate() then return end
 	if GetNumQuestChoices() == 1 then
 		GetQuestReward(1)
 	end
